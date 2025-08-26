2187fd313aedcfdf0ea30b849fdc9e5d8febe2ed
thaiiss7 <thaismichel7@gmail.com>
2025-08-26 20:54:44 -0300

    add js code

---
+++ script.js
@@ -0,0 +1,49 @@
+//array com as imagens
+const images = [
+    './assests/image-content/image-1.png',
+    './assests/image-content/image-2.png',
+    './assests/image-content/image-3.png',
+    './assests/image-content/image-4.png',
+    './assests/image-content/image-5.png',
+    './assests/image-content/image-6.png'
+];
+
+//conecta os elementos html que precisam ser manipulados
+const imageContent = document.querySelector('.image-content');
+const mainButton = document.getElementById('main-button');
+const finalMessage = document.querySelector(',final-message');
+
+//procura a imagem atual
+let currentIndex = 0;
+
+function updateImage() {
+    imageContent.style.opacity = 0;
+
+    const img = new Image();
+    img.src = images[currentIndex];
+
+    img.onload = () => {
+        imageContent.style.backgroundImage = `url('${images[currentIndex]}')`;
+
+        imageContent.style.opacity = 1;
+    };
+}
+
+//mostra a primeira imagem
+updateImage();
+
+//configura o botão
+mainButton.addEventListener('click', () => {
+    currentIndex++;
+
+    //se não estiver no final
+    if (currentIndex < images.length) {
+        updateImage();
+    }
+
+    //se estiver na ultima imagem
+    if (currentIndex == images.length - 1) {
+        mainButton.style.display = 'none';
+        finalMessage.style.display = 'block';
+    }
+});
