d928021f39fb213d94a7b2e0a99ba937a926b2f7
thaiiss7 <thaismichel7@gmail.com>
2025-08-26 21:03:28 -0300

    fix button

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //array com as imagens
 const images = [
-    './assests/image-content/image-1.png',
-    './assests/image-content/image-2.png',
-    './assests/image-content/image-3.png',
-    './assests/image-content/image-4.png',
-    './assests/image-content/image-5.png',
-    './assests/image-content/image-6.png'
+    './assets/image-content/image-1.png',
+    './assets/image-content/image-2.png',
+    './assets/image-content/image-3.png',
+    './assets/image-content/image-4.png',
+    './assets/image-content/image-5.png',
+    './assets/image-content/image-6.png'
 ];
 
 //conecta os elementos html que precisam ser manipulados
 const imageContent = document.querySelector('.image-content');
 const mainButton = document.getElementById('main-button');
-const finalMessage = document.querySelector(',final-message');
+const finalMessage = document.querySelector('.final-message');
 
 //procura a imagem atual
 let currentIndex = 0;
@@ -42,7 +42,7 @@ mainButton.addEventListener('click', () => {
     }
 
     //se estiver na ultima imagem
-    if (currentIndex == images.length - 1) {
+    if (currentIndex === images.length - 1) {
         mainButton.style.display = 'none';
         finalMessage.style.display = 'block';
     }
